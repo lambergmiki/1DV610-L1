12e93a1b2bf7a0535b95aeb37aa52a1a1530dd1e
Miki <ml227cr@student.lnu.se>
2025-09-05 23:47:59 +0200

    add complete raffle-method which displays text or gif based on user input

--- raffle.js
+++ raffle.js
@@ -1,3 +1,39 @@
+const inputField = document.querySelector('#input-field');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    inputField.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            raffle()
+        }
+    });
+});
+
+
 function raffle() {
-    // TODO: take user input and output distorted name, different elements based on input
+    const name = inputField.value.trim()
+
+    const existingResult = document.querySelector('h2')
+    if (existingResult) existingResult.remove()
+
+    const existingImg = document.querySelector('img');
+    if (existingImg) existingImg.remove();
+
+
+    const textResult = document.createElement('h2')
+
+
+    if (name.includes("dance")) {
+        textResult.textContent = `YOU WON! WOOOH.`
+        document.body.appendChild(textResult)
+        const prize = document.createElement('img')
+        prize.src = './images/boyle_rocks.gif'
+        document.body.appendChild(prize)
+        inputField.value = ""
+    } else {
+        textResult.textContent = `Hello, ${name}. You did not win the raffle.`
+        document.body.appendChild(textResult)
+    }
+
+    inputField.value = ""
 }
